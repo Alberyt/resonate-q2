3e2dbfb57b4d662d7c57a2f342f58ee8859d33b3
Albert <albert.li1@outlook.com>
2024-05-26 22:48:34 +1000

    added: handleContact enables more details about contact to be displayed

--- components/ui/contacts/quick-view.tsx
+++ components/ui/contacts/quick-view.tsx
@@ -2,6 +2,7 @@ import { IContact } from "@/models"
 
 import DisplayPicture from "./display-picture";
 import IconButton from "../icon-button";
+import useFetchContact from "@/hooks/useFetchContact";
 
 type View = {
   contacts?: IContact[],
@@ -10,26 +11,27 @@ type View = {
 }
 
 const QuickView: React.FC<View> = ({contacts, selected, updateSelected}) => {
+  const {handleContact} = useFetchContact();
   return (
     <>
       {contacts && 
         <div className="flex flex-col rounded-lg w-full min-h-fit py-2 space-y-2">
           {contacts?.map((contact, index) => (
-            <div key={index} className={`ml-2 mx-auto items-center ${contacts.length - 1 !== index && "pb-2"} w-full`}>
-              <div tabIndex={0} className={`flex items-center space-x-4 hover:cursor-pointer`} onClick={() => updateSelected(contact.id)}>
+            <div key={index} className={`ml-2 mx-auto items-center ${contacts.length - 1 !== index && "pb-2"} w-full hover:shadow-lg rounded-3xl`}>
+              <div tabIndex={0} className={`flex items-center space-x-4 hover:cursor-pointer ml-2`} onClick={() => updateSelected(contact.id)}>
                 <DisplayPicture contact={contact}/>
                 <div className="flex flex-col">
                   <h1 className="truncate overflow-hidden">{contact.name}</h1>
-                  {selected === contact.id && <p className="font-light text-xs flex justify-center">Mobile: {contact.mobile}</p>}
+                  {selected === contact.id && <p className="font-light text-xs flex justify-center text-gray-500">Mobile: {contact.mobile}</p>}
                 </div>
               </div>
               {selected === contact.id && (
-                <div className="pt-2 space-y-2">
-                  <section className="flex justify-between w-5/6">
-                    <IconButton symbol={"call"} />
+                <div className="pt-4 space-y-2 ml-2">
+                  <section className="flex justify-between w-[200px]">
+                    <IconButton symbol={"call"} className="bg-green-400 hover:bg-green-600"/>
                     <IconButton symbol={"message"} className="bg-blue-400 hover:bg-blue-600" />
-                    <IconButton symbol={"video"} />
-                    <IconButton symbol={"more"} className="bg-orange-400 hover:bg-orange-600"/>
+                    <IconButton symbol={"video"} className="bg-green-400 hover:bg-green-600"/>
+                    <IconButton symbol={"more"} className="bg-orange-400 hover:bg-orange-600" handleContact={() => handleContact(contact.id)}/>
                   </section>
                 </div>
               )}
