da5856a829aa66f054bef192ff9183a37480948a
Albert <albert.li1@outlook.com>
2024-05-27 01:16:53 +1000

    updated: changed more to view, fixed button to only display placeholder on unimplemented functions

--- components/ui/icon-button.tsx
+++ components/ui/icon-button.tsx
@@ -27,9 +27,12 @@ const IconButton: React.FC<Icon> = ({symbol, displayLabel = true, className, han
       <div className="flex flex-col space-y-2 items-center">
           <div className={cn("h-8 w-8 rounded-full inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90", className)} 
             onClick={handleContact}>
-            <Placeholder>
-              {icon}
-            </Placeholder>
+            {handleContact ? 
+              <>{icon}</> :
+              <Placeholder>
+                {icon}
+              </Placeholder>
+            }
           </div>
         { displayLabel && <h1 className="text-xs capitalize">{symbol}</h1> }
       </div>
@@ -46,7 +49,7 @@ const createIcon = (symbol: string) => {
       return <MdMessage {...options} />
     case "video":
       return <MdVideoCall {...options} />
-    case "more":
+    case "view":
       return <MdMoreHoriz {...options} />
     case "edit":
       return <MdMoreHoriz {...options} />
