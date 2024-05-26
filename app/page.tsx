fa77c0bd84aa03c3563a390905a7fffa809adc4b
Albert <albert.li1@outlook.com>
2024-05-26 22:53:19 +1000

    update: added contact section for layout, made it more responsive for mobile devices

--- app/page.tsx
+++ app/page.tsx
@@ -10,22 +10,31 @@ export default function Home() {
   const { contacts, contact, favourites } = useContact();
   return (
     <main className="h-screen w-full">
-      <Header />
-      <div className="flex">
-        <section className="h-[calc(100vh-64px)] w-full md:w-[400px] md:p-6">
-          <Contacts contacts={contacts}>
-            <div className="block md:hidden bg-gray-300 rounded-3xl space-y-4">
-              <Favourites favourites={favourites}/>
-            </div>
-          </Contacts>
-        </section>
-        <section className="hidden md:block w-[calc(100vw-400px)] h-full">
-          <div className="bg-gray-300 my-6 mr-6 rounded-3xl p-6 space-y-4">
-            <Favourites favourites={favourites}/>
+        {contact.contact && (
+          // Creates an overlay above contact list on smaller screens when a contact is selected.
+          <div className="block md:hidden absolute z-10 h-full w-full">   
+            <Contact />
           </div>
-          {/* <Contact /> */}
-        </section>
-      </div>
+        )}
+          <Header />
+          <div className="flex h-[calc(100vh-64px)]">
+            <section className="w-full md:w-[400px] md:p-6">
+              <Contacts contacts={contacts}>
+                <div className="block md:hidden bg-gray-300 rounded-3xl space-y-4">
+                  <Favourites favourites={favourites}/>
+                </div>
+              </Contacts>
+            </section>
+            <section className="hidden md:block w-[calc(100vw-400px)] h-full">
+              <div className="h-48 bg-gray-300 my-6 mr-6 rounded-3xl p-6 space-y-4">
+                <Favourites favourites={favourites}/>
+              </div>
+              {/*             Viewport - (header + favourites + padding) */}
+              <div className="h-[calc(100vh-(64px+192px+72px))]"> 
+                <Contact />
+              </div>
+            </section>
+          </div>
     </main>
   );
 }
