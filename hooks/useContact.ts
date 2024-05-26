36d0f3150f7a0cc579fde68ba1e763b8e627be2f
Albert <albert.li1@outlook.com>
2024-05-26 15:46:12 +1000

    added: useFetchFavourites

--- hooks/useContact.ts
+++ hooks/useContact.ts
@@ -1,14 +1,17 @@
 import useFetchContacts from "./useFetchContacts";
 import useFetchContact from "./useFetchContact";
+import useFetchFavourites from "./useFetchFavourites";
 
 
 const useContact = () => {
   const contacts = useFetchContacts();
   const contact = useFetchContact();
+  const favourites = useFetchFavourites();
 
   return {
     contacts,
-    contact
+    contact,
+    favourites
   }
 }
 
