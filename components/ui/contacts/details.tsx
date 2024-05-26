1c15c59a999b7febdc07094fd5821c383b15bdf5
Albert <albert.li1@outlook.com>
2024-05-26 22:40:12 +1000

    Displays the contact's details when selected

---
+++ components/ui/contacts/details.tsx
@@ -0,0 +1,18 @@
+import { IContact } from "@/models"
+
+const Details: React.FC<IContact> = ({mobile, email, website, notes }) => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full lg:w-1/2">
+      <h1>Mobile:</h1>
+      <h1>{mobile}</h1>
+      <h1>Email:</h1>
+      <h1>{email}</h1>
+      <h1>Website:</h1>
+      <h1>{website}</h1>
+      <h1>Notes:</h1>
+      <h1>{notes}</h1>
+    </div>
+  )
+}
+
+export default Details
