b77f057eb2ac286ccf2863bda92e1f90ae32f7c4
Albert <albert.li1@outlook.com>
2024-05-26 22:45:39 +1000

    style: minor changes for smaller screens

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -2,11 +2,11 @@ import { Separator } from "./separator"
 
 const Header = () => {
   return (
-    <div className="px-6">
-      <div className="flex flex-row items-center w-full h-12">
-        <h1 className="text-xl">Welcome back, <span className="text-red-600 hover:underline hover:cursor-pointer">User!</span></h1>
+    <div className="px-6 bg-gray-300 md:bg-white">
+      <div className="flex flex-row items-center w-full h-14">
+        <h1 className="text-xl ml-2">Welcome back, <span className="text-red-600 hover:underline hover:cursor-pointer">User!</span></h1>
       </div>
-      <Separator className="bg-black"/>
+      <Separator className="bg-gray-400"/>
     </div>
   )
 }
