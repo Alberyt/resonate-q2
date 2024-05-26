82252173b5660ed0af98f22be38481f38a3dc404
Albert <albert.li1@outlook.com>
2024-05-26 22:50:32 +1000

    Created contact component which displays more details about the selected contact

--- components/layout/contact.tsx
+++ components/layout/contact.tsx
@@ -1,9 +1,43 @@
 
+import { FaArrowLeft } from "react-icons/fa";
+import DisplayPicture from "../ui/contacts/display-picture";
+import IconButton from "../ui/icon-button";
+import { ScrollArea } from "../ui/scroll-area";
+import { Separator } from "../ui/separator";
+import Details from "../ui/contacts/details";
+import { Button } from "../ui/button";
+import useContact from "@/hooks/useContact";
 
 const Contact = () => {
+  const { contact } = useContact();
   return (
-    <section>
-      Contact
+    <section className="bg-gray-300 md:my-6 md:mr-6 md:rounded-3xl p-8 h-full z">
+      <ScrollArea className="h-full">
+        {contact.contact && (
+          <section className="flex flex-col space-y-6">
+            <Button variant={"link"} size={"icon"} onClick={contact.removeContact} className="ml-2 md:hidden">
+            <FaArrowLeft size={20}/>
+            </Button>
+            <div className="flex flex-col lg:flex-row items-center space-y-4 lg:space-y-0 lg:space-x-16">
+              <DisplayPicture contact={contact.contact} variant="large"/>
+              <div className="flex flex-col gap-1 w-full text-center lg:text-start">
+                <h1 className="text-lg font-semibold">{contact.contact.name}</h1>
+                <p className="text-gray-500">Mobile: {contact.contact.mobile}</p>
+                <div className="flex flex-row pt-2 space-y-2 justify-center lg:justify-start">
+                  <section className="flex justify-between w-full md:w-[250px]">
+                    <IconButton symbol={"call"} className="bg-green-400 hover:bg-green-600"/>
+                    <IconButton symbol={"message"} className="bg-blue-400 hover:bg-blue-600" />
+                    <IconButton symbol={"video"} className="bg-green-400 hover:bg-green-600"/>
+                    <IconButton symbol={"more"} className="bg-orange-400 hover:bg-orange-600"/>
+                  </section>
+                </div>
+              </div>
+            </div>
+            <Separator className="bg-gray-400"/>
+            <Details {...contact.contact}/>
+          </section>
+        )}
+      </ScrollArea>
     </section>
   )
 }
