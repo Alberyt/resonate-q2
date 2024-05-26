1f3423c3e67f3ca6f8795262aa49d6b9b1749cdf
Albert <albert.li1@outlook.com>
2024-05-27 00:30:08 +1000

    update: can now view user's contact details

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,3 +1,4 @@
+import useContact from "@/hooks/useContact"
 import { Separator } from "./separator"
 
 /**
@@ -5,10 +6,12 @@ import { Separator } from "./separator"
  * @returns Header to welcome the user
  */
 const Header = () => {
+  const { contact } = useContact();
   return (
     <div className="px-6 bg-gray-300 md:bg-white">
       <div className="flex flex-row items-center w-full h-14">
-        <h1 className="text-xl ml-2">Welcome back, <span className="text-red-600 hover:underline hover:cursor-pointer">User!</span></h1>
+        <h1 className="text-xl ml-2">Welcome back, <span className="text-red-600 hover:underline hover:cursor-pointer" 
+          onClick={() => contact.handleContact(100)}>Albert!</span></h1>
       </div>
       <Separator className="bg-gray-400"/>
     </div>
