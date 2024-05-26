fed97461b1f493deef8284884400aa880bbf84e6
Albert <albert.li1@outlook.com>
2024-05-26 15:50:40 +1000

    updated: implemented favourite list

--- components/layout/favourite-list.tsx
+++ components/layout/favourite-list.tsx
@@ -1,9 +1,30 @@
+import { IContact } from "@/models";
+import DisplayPicture from "../ui/contacts/display-picture";
+import { ScrollArea, ScrollBar } from "../ui/scroll-area";
+import { Button } from "../ui/button";
 
+type IFavourites = {
+  favourites: {
+    contacts: IContact[];
+    loading: boolean;
+  }
+}
 
-const Favourites = () => {
+const Favourites: React.FC<IFavourites> = ({favourites}) => {
   return (
     <section>
-      Favourites
+      <h1 className="font-semibold underline-offset-2">Favourites</h1>
+      <ScrollArea>
+        <div className="flex flex-row justify-start items-center space-x-4 mb-4">
+          {favourites.contacts.map((contact, index) => (
+            <Button key={index} className="flex flex-col h-full items-center bg-gray-300 hover:bg-gray-500" tabIndex={0}>
+              <DisplayPicture contact={contact} variant="medium"/>
+              <h1 className="text-sm text-black">{contact.name.split(" ")[0]}</h1>
+            </Button>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </section>
   )
 }
